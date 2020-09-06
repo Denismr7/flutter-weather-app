8158ebe0f351ecc20b46171cc696432b25f20422
Denis Mory <denismr7@gmail.com>
2020-09-06 10:40:11 +0200

    model and repo created

---
+++ lib/WeatherRepo.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:weather_app/WeatherModel.dart';
+
+class WeatherRepo {
+  Future<WeatherModel> getWeather(String city) async {
+    final String url =
+        "http://api.openweathermap.org/data/2.5/weather?q=$city&appid=140d4b2928deaa7118ad35110bcbc50c&units=metric";
+
+    final result = await http.Client().get(url);
+
+    if (result.statusCode != 200) throw Exception();
+
+    return parseJSON(result.body);
+  }
+
+  WeatherModel parseJSON(final response) {
+    final jsonDecoded = json.decode(response);
+    final jsonWeather = jsonDecoded["main"];
+
+    return WeatherModel.fromJson(jsonWeather);
+  }
+}
